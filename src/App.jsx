ed00ec2a1f912453bc571a70878aa8f2ef84bd27
Sulaimon Taofik <Sulaimontaofik08@gmail.com>
2024-03-18 01:05:09 +0100

    Added the products but got ahold with some technicalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import './App.css'
-import Home from './component/Home'
-import Navbar from './component/Navbar'
-
+import "./App.css";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import Products from "./component/Products";
+import {Switch, Route} from "react-router-dom";
 
 function App() {
-
   return (
     <>
-     <Navbar/>
-     <Home/>
-     
+    <Navbar />
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+      </Switch>
+      console.log('I am a boy');
     </>
-  )
+  );
 }
 
-export default App
+export default App;
