09742b3272414a3676907d55ce2d61667af130f8
Sulaimon Taofik <Sulaimontaofik08@gmail.com>
2024-03-21 23:33:32 +0100

    made the skeleton works

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./component/Navbar";
 import Products from "./component/Products";
 import Product from "./component/Product";
 import Cart from "./component/Cart";
-import { Routes, Route, } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
